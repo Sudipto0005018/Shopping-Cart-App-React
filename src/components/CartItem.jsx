69cf5b85566f9bd3bba1d100e7be350f10fdbac8
Sudipto Dutta <duttasudipto11@gmail.com>
2025-02-16 10:02:18 +0530

    2025 changes

--- src/components/CartItem.jsx
+++ src/components/CartItem.jsx
@@ -1,41 +1,79 @@
 
-import {FcDeleteDatabase} from "react-icons/fc"
+// import {FcDeleteDatabase} from "react-icons/fc"
+// import { useDispatch } from "react-redux";
+// import { remove } from "../redux/Slices/CartSlice";
+// import { toast } from "react-hot-toast";
+
+// const CartItem = ({item, itemIndex}) => {
+//   const dispatch = useDispatch();
+
+//   const removeFromCart = () => {
+//     dispatch(remove(item.id));
+//     toast.error("Item Removed");
+//   }
+
+//   return (
+//     <div>
+
+//       <div>
+
+//         <div>
+//           <img src={item.image} />
+//         </div>
+//         <div>
+//           <h1>{item.title}</h1>
+//           <h1>{item.description}</h1>
+//           <div>
+//             <p>{item.price}</p>
+//             <div
+//             onClick={removeFromCart}>
+//               <FcDeleteDatabase/>
+//             </div>
+//           </div>
+
+//         </div>
+
+
+//       </div>
+
+//     </div>
+//   );
+// };
+
+// export default CartItem;
+
+
+
+
+import { FcDeleteDatabase } from "react-icons/fc";
 import { useDispatch } from "react-redux";
 import { remove } from "../redux/Slices/CartSlice";
 import { toast } from "react-hot-toast";
+import "./CartItem.css"; // Add this line
 
-const CartItem = ({item, itemIndex}) => {
+const CartItem = ({ item, itemIndex }) => {
   const dispatch = useDispatch();
 
   const removeFromCart = () => {
     dispatch(remove(item.id));
-    toast.success("Item Removed");
-  }
+    toast.error("Item Removed");
+  };
 
   return (
-    <div>
-
-      <div>
-
-        <div>
-          <img src={item.image} />
-        </div>
-        <div>
-          <h1>{item.title}</h1>
-          <h1>{item.description}</h1>
-          <div>
-            <p>{item.price}</p>
-            <div
-            onClick={removeFromCart}>
-              <FcDeleteDatabase/>
-            </div>
-          </div>
-
-        </div>
-
-
+    <div className="cart-item-card">
+      <div className="cart-item-image">
+        <img src={item.image} alt={item.title} />
       </div>
-
+      <div className="cart-item-details">
+        <h1>{item.title}</h1>
+        <p>{item.description}</p>
+        <div className="cart-item-price-remove">
+          <p>${item.price}</p>
+          <div className="remove-icon" onClick={removeFromCart}>
+            <FcDeleteDatabase />
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
--- src/pages/Cart.jsx
+++ src/pages/Cart.jsx
@@ -1,67 +1,124 @@
+// import { useEffect, useState } from "react";
+// import { useSelector } from "react-redux";
+// import { Link } from "react-router-dom";
+// import CartItem from "../components/CartItem";
+
+
+
+
+// const Cart = () => {
+
+//   const {cart} = useSelector((state) => state);
+//   console.log("Printing Cart");
+//   console.log(cart);
+//   const [totalAmount, setTotalAmount] = useState(0);
+
+//   useEffect( () => {
+//     setTotalAmount( cart.reduce( (acc, curr) => acc + curr.price,0) );
+//   }, [cart])
+
+//   return (
+//     <div>
+//   {
+//     cart.length > 0  ? 
+//     (<div>
+
+
+//       <div>
+//         {
+//           cart.map( (item,index) => {
+//             return <CartItem key={item.id} item={item} itemIndex={index} />
+//           } )
+//         }
+//       </div>
+
+//       <div>
+
+//         <div>
+//           <div>Your Cart</div>
+//           <div>Summary</div>
+//           <p>
+//             <span>Total Items: {cart.length}</span>
+//           </p>
+//         </div>
+
+//         <div>
+//           <p>Total Amount: ${totalAmount}</p>
+//           <button>
+//             CheckOut Now
+//           </button>
+//         </div>
+
+//       </div>
+
+
+//     </div>) : 
+//     (<div>
+//       <h1>Cart Empty</h1>
+//       <Link to={"/"}>
+//         <button>
+//           Shop Now
+//         </button>
+//       </Link>
+//     </div>)
+//   }
+//     </div>
+//   );
+// };
+
+// export default Cart;
+
+
+
 import { useEffect, useState } from "react";
 import { useSelector } from "react-redux";
 import { Link } from "react-router-dom";
 import CartItem from "../components/CartItem";
-
-
-
+import "./Cart.css"; // Add this line
 
 const Cart = () => {
-
-  const {cart} = useSelector((state) => state);
+  const { cart } = useSelector((state) => state);
   console.log("Printing Cart");
   console.log(cart);
   const [totalAmount, setTotalAmount] = useState(0);
 
-  useEffect( () => {
-    setTotalAmount( cart.reduce( (acc, curr) => acc + curr.price,0) );
-  }, [cart])
+  useEffect(() => {
+    setTotalAmount(cart.reduce((acc, curr) => acc + curr.price, 0));
+  }, [cart]);
 
   return (
-    <div>
-  {
-    cart.length > 0  ? 
-    (<div>
+    <div className="cart-container">
+      {cart.length > 0 ? (
+        <div className="cart">
+          <div className="cart-items">
+            {cart.map((item, index) => (
+              <CartItem key={item.id} item={item} itemIndex={index} />
+            ))}
+          </div>
 
+          <div className="cart-summary">
+            <div className="summary-details">
+              <div>Your Cart</div>
+              <div>Summary</div>
+              <p>
+                <span>Total Items: {cart.length}</span>
+              </p>
+            </div>
 
-      <div>
-        {
-          cart.map( (item,index) => {
-            return <CartItem key={item.id} item={item} itemIndex={index} />
-          } )
-        }
-      </div>
-
-      <div>
-
-        <div>
-          <div>Your Cart</div>
-          <div>Summary</div>
-          <p>
-            <span>Total Items: {cart.length}</span>
-          </p>
+            <div className="summary-total">
+              <p>Total Amount: ${totalAmount}</p>
+              <button className="checkout-button">CheckOut Now</button>
+            </div>
+          </div>
         </div>
-
-        <div>
-          <p>Total Amount: ${totalAmount}</p>
-          <button>
-            CheckOut Now
-          </button>
+      ) : (
+        <div className="cart-empty">
+          <h1>Cart Empty</h1>
+          <Link to={"/"}>
+            <button className="shop-now-button">Shop Now</button>
+          </Link>
         </div>
-
-      </div>
-
-
-    </div>) : 
-    (<div>
-      <h1>Cart Empty</h1>
-      <Link to={"/"}>
-        <button>
-          Shop Now
-        </button>
-      </Link>
-    </div>)
-  }
+      )}
     </div>
   );
 };
